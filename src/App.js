0bc65e571812045f379d8e40cc0592d100d458d8
yelab07 <79489573+yelab07@users.noreply.github.com>
2021-10-14 20:40:07 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import Navbar from './Components/NavBar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        {/* <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch> */}
+      </Router>
     </div>
   );
 }
---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./NavStyles";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h3>My Shop</h3>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/contact-us" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
